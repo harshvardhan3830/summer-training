ea92efa54c3c627dcd9ebdcbc437ea2b204be322
HarshVardhan01 <vardhan3830@gmail.com>
2024-08-01 22:07:31 +0530

    react project init

--- javascript/ArrayMethods.js
+++ javascript/ArrayMethods.js
@@ -1,10 +1,34 @@
 const arr = [];
 
-for(let i=1; i<= 20; i++){
-    arr.push(i);
+for (let i = 1; i <= 20; i++) {
+  arr.push(i);
 }
 
 //function which accept function as an argument are higher order functions
-arr.forEach((el, index) => {
-    console.log(el)
-})
+let tempArr = arr.forEach((el, index) => el);
+
+console.log("");
+let tempArrMap = arr.map((el, index) => el);
+
+// console.log(
+//   "Array from for each ====================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ",
+//   tempArr
+// );
+// console.log(
+//   "Array from map ====================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ",
+//   tempArrMap
+// );
+// let arrFilter = arr.filter((el, index) => el % 5 === 0);
+// console.log(arrFilter);
+
+// let arrPop = arr.pop();
+// console.log(arrPop);
+// console.log(arr);
+
+let arrShift = arr.shift(0);
+console.log(arrShift);
+console.log(arr);
+
+let arrUnShift = arr.unshift(2);
+console.log(arrUnShift);
+console.log(arr);
---
+++ react/expense-tracker/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className=" bg-slate-900 h-[100vh] text-white">App</div>;
+};
+
+export default App;
